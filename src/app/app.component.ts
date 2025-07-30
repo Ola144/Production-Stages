a983422855f85b28885a7494678c8827ad97ba39
Ola <oladimejiagbaje@gmail.com>
2025-07-30 15:45:14 +0100

    initial-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,68 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'production-stages';
+  newItem: string = '';
+
+  idea: string[] = [];
+  prototype: string[] = [];
+  development: string[] = [];
+  ship: string[] = [];
+
+  clickTimeout: any;
+
+  addNewItem() {
+    this.idea.push(this.newItem);
+    console.log(this.idea);
+    this.newItem = '';
+  }
+
+  movingFoward(stage: string, index: number) {
+    clearTimeout(this.clickTimeout);
+    let item = '';
+    switch (stage) {
+      case 'idea':
+        item = this.idea.splice(index, 1)[0];
+        this.prototype.push(item);
+        break;
+      case 'prototype':
+        this.clickTimeout = setTimeout(() => {
+          item = this.prototype.splice(index, 1)[0];
+          this.development.push(item);
+        }, 500);
+        break;
+      case 'development':
+        this.clickTimeout = setTimeout(() => {
+          item = this.development.splice(index, 1)[0];
+          this.ship.push(item);
+        }, 500);
+        break;
+    }
+  }
+
+  movingBackward(stage: string, index: number) {
+    clearTimeout(this.clickTimeout);
+    let item = '';
+    switch (stage) {
+      case 'prototype':
+        item = this.prototype.splice(index, 1)[0];
+        this.idea.push(item);
+        break;
+      case 'development':
+        item = this.development.splice(index, 1)[0];
+        this.prototype.push(item);
+        break;
+      case 'ship':
+        item = this.ship.splice(index, 1)[0];
+        this.development.push(item);
+        break;
+    }
+  }
 }
